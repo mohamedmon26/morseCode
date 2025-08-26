23d1d070d19428e08e883e669ca362b24a4c0b5c
Mohamed Montaser <mohamedmon26@users.noreply.github.com>
2025-08-27 01:58:49 +0300

    some comments

---
+++ encoder.py
@@ -0,0 +1,8 @@
+# This is a project to make a morse code encoder and deceoder
+# might be used for urgent communication
+prompt = input("Enter a message to encode: ")
+mapping = {
+    # gonna learn about morse code rn
+
+
+}
